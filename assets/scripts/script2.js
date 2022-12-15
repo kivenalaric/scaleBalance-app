42d9fdee69ba210fa9e33522989e5eaa893599bd
Kiven Alaric <elmozaro3@gmail.com>
2022-12-15 08:00:40 -0600

    edits on js code

--- assets/scripts/script2.js
+++ assets/scripts/script2.js
@@ -8,25 +8,26 @@ calculate.addEventListener('click', (e) => {
   if (element1 === '') {
     alert('Please input two weights to balance')
   }
-  if (element2 === '') {
+  else if (element2 === '') {
     alert('Please input optional weights to select from for scale balancing')
   }
   const elementoneArr = element1.split(',').map(Number)
   const elementtwoArr = element2.split(',').map(Number)
 
   if (elementoneArr.length !== 2 || elementoneArr[0] === 0) {
-    alert(
+    display.innerHTML = (
       'Weight added is less or more than two integers. Only two comma seperated weights can be added'
     )
-  }
+  } else {
   // the scale balancing is processed by calling the balanceScale function
   const result2 = balanceScale(elementoneArr, elementtwoArr)
   display.innerHTML = result
   return result2
+  }
 })
 
 function balanceScale (elementone, elementtwo) {
-  let balance = false
+  let balance
   const diff = elementone[1] - elementone[0]
   if (diff === 0) {
     result = 'scale is balanced'
@@ -48,12 +49,11 @@ function balanceScale (elementone, elementtwo) {
       } else if (elementtwo[i] + diff === elementtwo[j + 1]) {
         result = [elementtwo[i], elementtwo[j + 1]]
         balance = true
-      }
-      else{
-        result = `no number available to balance`
+        return result
+      } else {
+        result = 'no number available to balance'
         balance = false
       }
     }
   }
-  return result
 }
